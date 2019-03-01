3609974c80e832280b5a0eda0eb2d4792a02cd56
Moshe Nahmias <moshegrey@gmail.com>
2019-03-01 20:38:08 +0200

    tmp

--- setup.py
+++ setup.py
@@ -6,21 +6,11 @@ setup(
     name="HelloWorld"
     version="0.1",
     packages=find_packages(),
-    description="this is a description",
-    long_description="this is a very long description in markdown",
     download_url="URL",
     license="GPL",
-    test_suite="tests",
-    tests_require=["mock"],
-    zip_safe=False,
     python_requires="2.7, 3.6",
     project_urls={
         "bug tracker": "URL",
         "documentation": "URL"
-    }
-    entry_points={
-        'console_scripts': [
-            'hello': 'hello_world:main'
-        ]
     }
 )
