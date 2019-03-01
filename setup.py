1130767081d92a71fd732e77374e93ce8edd2a7b
Moshe Nahmias <moshegrey@gmail.com>
2019-03-01 10:57:31 +0200

    tmp

--- setup.py
+++ setup.py
@@ -1,21 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    maintainer="maintainer name",
-    maintainer_email="maintainer@example.com",
     name="HelloWorld"
     version="0.1",
     packages=find_packages(),
-    description="this is a description",
-    long_description="this is a very long description in markdown",
-    download_url="URL",
-    license="GPL",
-    test_suite="tests",
-    tests_require=["mock"],
-    zip_safe=False,
-    entry_points={
-        'console_scripts': [
-            'hello': 'hello_world:main'
-        ]
-    }
 )
