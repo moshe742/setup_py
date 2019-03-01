c221df585333eb15645258cf7c740fd403a60e91
Moshe Nahmias <moshegrey@gmail.com>
2019-03-01 20:24:23 +0200

    tmp

--- setup.py
+++ setup.py
@@ -6,16 +6,6 @@ setup(
     name="HelloWorld"
     version="0.1",
     packages=find_packages(),
-    description="this is a description",
-    long_description="this is a very long description in markdown",
     download_url="URL",
-    license="GPL",
-    test_suite="tests",
-    tests_require=["mock"],
-    zip_safe=False,
-    entry_points={
-        'console_scripts': [
-            'hello': 'hello_world:main'
-        ]
-    }
+    python_requires='2.7, >=3.6'
 )
