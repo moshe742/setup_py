cb319910a37203f5443203b7921d37c6fe9901d1
Moshe Nahmias <moshegrey@gmail.com>
2019-03-01 21:23:28 +0200

    tmp

--- setup.py
+++ setup.py
@@ -8,16 +8,14 @@ setup(
     packages=find_packages(),
     description="this is a description",
     long_description="this is a very long description in markdown",
+    long_description_content_type="text/markdown",
     url="URL",
     license="GPL",
-    test_suite="tests",
-    tests_require=["mock"],
-    zip_safe=False,
     python_requires="2.7, 3.6",
     project_urls={
         "bug tracker": "URL",
         "documentation": "URL"
-    }
+    },
     entry_points={
         'console_scripts': [
             'hello': 'hello_world:main'
