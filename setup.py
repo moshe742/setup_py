6af42b914d2ac6634a89117a2cf9ff455da2f6aa
Moshe Nahmias <moshegrey@gmail.com>
2019-03-02 16:23:23 +0200

    tmp

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 setup(
     maintainer="maintainer name",
     maintainer_email="maintainer@example.com",
-    name="HelloWorld"
+    name="HelloWorld",
     version="0.1",
     packages=find_packages(),
     url="URL",
-    python_requires="2.7, 3.6",
+    python_requires=">=2.7.13, >=3.6",
     project_urls={
         "bug tracker": "URL",
         "documentation": "URL"
